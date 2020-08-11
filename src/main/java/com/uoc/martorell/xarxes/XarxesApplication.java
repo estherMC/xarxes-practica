98197caa49cb663d1726f783771ced46a391f92d
EstherMC <esther_3@hotmail.com>
2020-08-11 22:45:14 +0200

    :sparkles: First commit projecte xarxes

---
+++ src/main/java/com/uoc/martorell/xarxes/XarxesApplication.java
@@ -0,0 +1,13 @@
+package com.uoc.martorell.xarxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XarxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XarxesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uoc/martorell/xarxes/XarxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoc.martorell.xarxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XarxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
